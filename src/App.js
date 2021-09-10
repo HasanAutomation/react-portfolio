17068e4df7c22a6376a22116bdfce08866a55c1f
Hasan Ali <hasan.ali@lacritz.com>
2021-09-10 22:38:44 +0530

    Added basic layout

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import './App.css';
+import './App.scss';
+import Contact from './components/Contact';
+import Intro from './components/Intro';
+import Navbar from './components/Navbar';
+import Portfolio from './components/Portfolio';
+import Testimonials from './components/Testimonials';
+import Works from './components/Works';
 
 function App() {
   return (
-    <div className="App">
-     App
+    <div className='App'>
+      <Navbar />
+      <div className='sections'>
+        <Intro />
+        <Portfolio />
+        <Works />
+        <Testimonials />
+        <Contact />
+      </div>
     </div>
   );
 }
