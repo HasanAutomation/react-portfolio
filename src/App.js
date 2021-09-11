8a4c62274e7087c3e39fcf23bd204c30dd6023b5
Hasan Ali <hasan.ali@lacritz.com>
2021-09-11 16:55:20 +0530

    Added responsive navigation menu

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
+import { useState } from 'react';
 import './App.scss';
 import Contact from './components/Contact';
 import Intro from './components/Intro';
+import Menu from './components/Menu';
 import Navbar from './components/Navbar';
 import Portfolio from './components/Portfolio';
 import Testimonials from './components/Testimonials';
 import Works from './components/Works';
 
 function App() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <div className='App'>
-      <Navbar />
+      <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+      <Menu menuOpen={menuOpen} />
       <div className='sections'>
         <Intro />
         <Portfolio />
