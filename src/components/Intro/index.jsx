aacfe586ee57547944a7501592c78349abb3cf55
Hasan Ali <hasan.ali@lacritz.com>
2021-09-12 22:30:53 +0530

    Added type writing effect

--- src/components/Intro/index.jsx
+++ src/components/Intro/index.jsx
@@ -1,9 +1,36 @@
+import { useEffect, useRef } from 'react';
+import { init } from 'ityped';
 import './Intro.scss';
 
 export default function Intro() {
+  const typeRef = useRef();
+
+  useEffect(() => {
+    init(typeRef.current, {
+      showCursor: true,
+      backDelay: 1500,
+      strings: ['Web', 'Mobile', 'Desktop'],
+    });
+  }, []);
   return (
     <div className='intro' id='intro'>
-      Intro
+      <div className='left'>
+        <div className='imgContainer'>
+          <img src='/assets/man.png' alt='Man' />
+        </div>
+      </div>
+      <div className='right'>
+        <div className='wrapper'>
+          <h2>Hi There,I'm</h2>
+          <h1>Hasan Ali</h1>
+          <h3>
+            I work as a <span ref={typeRef}></span> developer
+          </h3>
+        </div>
+        <a href='#portfolio'>
+          <img src='/assets/down.png' alt='Down' />
+        </a>
+      </div>
     </div>
   );
 }
