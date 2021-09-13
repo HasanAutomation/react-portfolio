64b75f179894f12efbfe56c3484961c3515e1d88
Hasan Ali <hasan.ali@lacritz.com>
2021-09-13 10:53:56 +0530

    Completed portfolio page

--- src/components/Portfolio/index.jsx
+++ src/components/Portfolio/index.jsx
@@ -3,7 +3,39 @@ import './Portfolio.scss';
 export default function Portfolio() {
   return (
     <div className='portfolio' id='portfolio'>
-      Portfolio
+      <h1>Portfolio</h1>
+      <ul>
+        <li className='active'>Mobile</li>
+        <li>Web</li>
+        <li>Desktop</li>
+        <li>Game</li>
+      </ul>
+      <div className='container'>
+        <div className='item'>
+          <img src='https://source.unsplash.com/random' alt='' />
+          <h3>Banking App</h3>
+        </div>
+        <div className='item'>
+          <img src='https://source.unsplash.com/random' alt='' />
+          <h3>Banking App</h3>
+        </div>
+        <div className='item'>
+          <img src='https://source.unsplash.com/random' alt='' />
+          <h3>Banking App</h3>
+        </div>
+        <div className='item'>
+          <img src='https://source.unsplash.com/random' alt='' />
+          <h3>Banking App</h3>
+        </div>
+        <div className='item'>
+          <img src='https://source.unsplash.com/random' alt='' />
+          <h3>Banking App</h3>
+        </div>
+        <div className='item'>
+          <img src='https://source.unsplash.com/random' alt='' />
+          <h3>Banking App</h3>
+        </div>
+      </div>
     </div>
   );
 }
